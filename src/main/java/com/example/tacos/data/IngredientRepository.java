d6f94224c49633a454479a84adda8f1bda3c7853
artgonzalez <artgonzalezjr@gmail.com>
2022-07-17 21:52:11 -0500

    Ingredient data, domain, web(controller) and view

---
+++ src/main/java/com/example/tacos/data/IngredientRepository.java
@@ -0,0 +1,7 @@
+package com.example.tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.tacos.domain.Ingredient;
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+}
---
+++ src/main/java/com/example/tacos/web/DesignTacoController.java
@@ -0,0 +1,50 @@
+package com.example.tacos.web;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.tacos.data.IngredientRepository;
+import com.example.tacos.domain.Ingredient;
+import com.example.tacos.domain.Ingredient.Type;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+	private final IngredientRepository ingredientRepo;
+
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+
+	@GetMapping
+	public String showDesignForm(Model model) {
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+		Type[] types = Ingredient.Type.values();
+		for (Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+		}
+
+		return "design";
+	}
+
+	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	}
+}
