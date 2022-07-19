d070f10ccd64175f1cbb4e23bd50e3375d596c2e
artgonzalez <artgonzalezjr@gmail.com>
2022-07-19 09:14:28 -0500

    Home page browser test

---
+++ src/test/java/com/example/tacos/web/test/HomePageBrowserTest.java
@@ -0,0 +1,56 @@
+package com.example.tacos.web.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class HomePageBrowserTest {
+
+  @LocalServerPort
+  private int port;
+  private static HtmlUnitDriver browser;  
+  
+  @BeforeAll
+  public static void setup() {
+    browser = new HtmlUnitDriver();
+    
+    browser.manage().timeouts()
+          .implicitlyWait(Duration.ofSeconds(10));
+  }
+  
+  @AfterAll
+  public static void teardown() {
+    browser.quit();
+  }
+  
+  @Test
+  public void testHomePage() {
+    String homePage = "http://localhost:" + port;
+    browser.get(homePage);
+    
+    String titleText = browser.getTitle();
+    assertEquals("Taco Cloud", titleText);
+    
+    String h1Text = browser.findElement(By.tagName("h1")).getText();
+    assertEquals("Welcome to...", h1Text);
+    
+    String imgSrc = browser.findElement(By.tagName("img"))
+                                              .getAttribute("src");
+    assertEquals(homePage + "/images/TacoCloud.png", imgSrc);
+  }
+  
+  
+}
