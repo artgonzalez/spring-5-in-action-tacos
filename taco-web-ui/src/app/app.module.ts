3b93a54de80bcddc8f268d50cf42be294c838bc4
artgonzalez <artgonzalezjr@gmail.com>
2022-07-23 22:18:08 -0500

    add cloud-title, home, header, and footer

--- taco-web-ui/src/app/app.module.ts
+++ taco-web-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CloudTitleComponent } from './cloud-title/cloud-title.component';
+import { HomeComponent } from './home/home.component';
+import { TacoHeaderComponent } from './taco-header/taco-header.component';
+import { TacoFooterComponent } from './taco-footer/taco-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CloudTitleComponent,
+    HomeComponent,
+    TacoHeaderComponent,
+    TacoFooterComponent
   ],
   imports: [
     BrowserModule,
