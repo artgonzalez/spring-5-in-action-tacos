d8a7cbcd5f811baa25a974d4e30fa3ad2e814191
artgonzalez <artgonzalezjr@gmail.com>
2022-08-01 09:42:42 -0500

    add ApiService and httpclient injecting configuration

--- taco-web-ui/src/app/app.module.ts
+++ taco-web-ui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CloudTitleComponent } from './cloud-title/cloud-title.component';
 import { HomeComponent } from './home/home.component';
 import { TacoHeaderComponent } from './taco-header/taco-header.component';
 import { TacoFooterComponent } from './taco-footer/taco-footer.component';
+import { ApiService } from './api/api.service'
 
 @NgModule({
   declarations: [
@@ -18,9 +20,10 @@ import { TacoFooterComponent } from './taco-footer/taco-footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
